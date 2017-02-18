7d41daf532a132660dbcf8aeef706e8c737ce24a
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-02-18 06:03:32 +0500

    Completed

--- 30DaysOfCode/Program.cs
+++ 30DaysOfCode/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// Name : Usama Tahir
+// Conditional Statements
+// HACKER RANK 30 Days of Challenge
+
+
+
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +17,22 @@ namespace _30DaysOfCode
     {
         static void Main(string[] args)
         {
+            int N = Convert.ToInt32(Console.ReadLine());
+            if(N%2!=0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if(N%2==0)
+            {
+                if (N >= 2 && N <= 5)
+                    Console.WriteLine("Not Weird");
+                else if (N >= 6 && N <= 20)
+                    Console.WriteLine("Weird");
+                else if(N>20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+            }
         }
     }
 }
