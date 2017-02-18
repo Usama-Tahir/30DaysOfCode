49a819a928049dfd7c794488ea461b112e074f73
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-02-18 06:34:36 +0500

    Day 4 completed

--- 30DaysOfCode/Program.cs
+++ 30DaysOfCode/Program.cs
@@ -1,38 +1,38 @@
-﻿// Name : Usama Tahir
-// Conditional Statements
-// HACKER RANK 30 Days of Challenge
+﻿//// Name : Usama Tahir
+//// Conditional Statements
+//// HACKER RANK 30 Days of Challenge
 
 
 
-using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.IO;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace _30DaysOfCode
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int N = Convert.ToInt32(Console.ReadLine());
-            if(N%2!=0)
-            {
-                Console.WriteLine("Weird");
-            }
-            else if(N%2==0)
-            {
-                if (N >= 2 && N <= 5)
-                    Console.WriteLine("Not Weird");
-                else if (N >= 6 && N <= 20)
-                    Console.WriteLine("Weird");
-                else if(N>20)
-                {
-                    Console.WriteLine("Not Weird");
-                }
-            }
-        }
-    }
-}
+//namespace _30DaysOfCode
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int N = Convert.ToInt32(Console.ReadLine());
+//            if(N%2!=0)
+//            {
+//                Console.WriteLine("Weird");
+//            }
+//            else if(N%2==0)
+//            {
+//                if (N >= 2 && N <= 5)
+//                    Console.WriteLine("Not Weird");
+//                else if (N >= 6 && N <= 20)
+//                    Console.WriteLine("Weird");
+//                else if(N>20)
+//                {
+//                    Console.WriteLine("Not Weird");
+//                }
+//            }
+//        }
+//    }
+//}
