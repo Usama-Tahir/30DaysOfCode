d6f5ef57bea8fff3c5ffdc925fc9163990308606
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-02-18 06:43:40 +0500

    Day 5 completed

--- 30DaysOfCode/Day4.cs
+++ 30DaysOfCode/Day4.cs
@@ -1,54 +1,54 @@
-﻿// Name : Usama Tahir
-// HACKER RANK 30 DAYS OF CHALLENGE
-// Classes vs Instances
+﻿//// Name : Usama Tahir
+//// HACKER RANK 30 DAYS OF CHALLENGE
+//// Classes vs Instances
 
 
-using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.IO;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-class Person
-{
-    public int age;
-    public Person(int initialAge)
-    {
-        if (initialAge >= 0)
-            this.age = initialAge;
-        else if (initialAge < 0)
-        {
-            this.age = 0;
-            Console.WriteLine("Age is not valid, setting age to 0.");
-        }   
-    }
-    public void yearPasses()
-    {
-        this.age++;
-    }
-    public void amIOld()
-    {
-        if (this.age < 13)
-            Console.WriteLine("You are young.");
-        else if (this.age >= 13 && this.age < 18)
-            Console.WriteLine("You are a teenager.");
-        else
-            Console.WriteLine("You are old.");
-    }
+//class Person
+//{
+//    public int age;
+//    public Person(int initialAge)
+//    {
+//        if (initialAge >= 0)
+//            this.age = initialAge;
+//        else if (initialAge < 0)
+//        {
+//            this.age = 0;
+//            Console.WriteLine("Age is not valid, setting age to 0.");
+//        }   
+//    }
+//    public void yearPasses()
+//    {
+//        this.age++;
+//    }
+//    public void amIOld()
+//    {
+//        if (this.age < 13)
+//            Console.WriteLine("You are young.");
+//        else if (this.age >= 13 && this.age < 18)
+//            Console.WriteLine("You are a teenager.");
+//        else
+//            Console.WriteLine("You are old.");
+//    }
 
-}
+//}
 
-namespace _30DaysOfCode
-{
-    class Day4
-    {
-        static void Main(string[] args)
-        {
-            int T = Convert.ToInt32(Console.ReadLine());
-            Person one = new Person(T);
+//namespace _30DaysOfCode
+//{
+//    class Day4
+//    {
+//        static void Main(string[] args)
+//        {
+//            int T = Convert.ToInt32(Console.ReadLine());
+//            Person one = new Person(T);
 
-        }
+//        }
 
-    }
-}
+//    }
+//}
---
+++ 30DaysOfCode/Day5.cs
@@ -0,0 +1,24 @@
+﻿// Loops
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DaysOfCode
+{
+    class Day5
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=10;i++)
+            {
+                int result = n * i;
+                Console.WriteLine(n + " x " + i + " = " + result);
+            }
+        }
+    }
+}
