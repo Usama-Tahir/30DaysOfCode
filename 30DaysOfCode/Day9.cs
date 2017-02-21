5cafc9e773a0b9316df86fe1a48c00f49c84860b
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-02-21 06:33:32 +0500

    hell yeah

---
+++ 30DaysOfCode/Day9.cs
@@ -0,0 +1,32 @@
+﻿// Name : Usama Tahir
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DaysOfCode
+{
+    
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(factorial(N));
+
+        }
+        static public int factorial(int n)
+        {
+            if (n <= 1)
+                return 1;
+            else
+            {
+                return n * factorial(n - 1);
+            }
+        }
+
+    }
+}
