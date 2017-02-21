62bbc336942f7dbe02e183b6c7143e79e1b7b7aa
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-02-21 06:37:25 +0500

    completed

--- 30DaysOfCode/Day8.cs
+++ 30DaysOfCode/Day8.cs
@@ -1,58 +1,58 @@
-﻿// Name: Usama Tahir
-// trying to get things traight
+﻿//// Name: Usama Tahir
+//// trying to get things traight
 
-using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.IO;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace _30DaysOfCode
-{
-    class Solution
-    {
-        static void Main(string[] args)
-        {
-            Dictionary<string, int> phoneBook = new Dictionary<string, int>();
-            IList<string> queryList = new List<string>();
-            int n = Convert.ToInt32(Console.ReadLine());
-            string tempName = "";
-            int tempNumb = 0;
-            for (int i = 1; i <= n; i++)
-            {
-                string[] arr = Console.ReadLine().Split(' ');
-                tempName = arr[0];
-                tempNumb = int.Parse(arr[1]);
-                phoneBook.Add(tempName, tempNumb);
-                //Console.WriteLine(tempName + "  " + tempNumb);
-            }
-            string input = "";
-            for (int l = 1; l <= n; l++)
-            {
-                input = Console.ReadLine();
-                queryList.Add(input);
+//namespace _30DaysOfCode
+//{
+//    class Solution
+//    {
+//        static void Main(string[] args)
+//        {
+//            Dictionary<string, int> phoneBook = new Dictionary<string, int>();
+//            IList<string> queryList = new List<string>();
+//            int n = Convert.ToInt32(Console.ReadLine());
+//            string tempName = "";
+//            int tempNumb = 0;
+//            for (int i = 1; i <= n; i++)
+//            {
+//                string[] arr = Console.ReadLine().Split(' ');
+//                tempName = arr[0];
+//                tempNumb = int.Parse(arr[1]);
+//                phoneBook.Add(tempName, tempNumb);
+//                //Console.WriteLine(tempName + "  " + tempNumb);
+//            }
+//            string input = "";
+//            for (int l = 1; l <= n; l++)
+//            {
+//                input = Console.ReadLine();
+//                queryList.Add(input);
 
-            }
-
-
-            foreach (string inp in queryList)
-            {
-                //Console.WriteLine(inp);
-                if (phoneBook.ContainsKey(inp))
-                {
-                    Console.WriteLine(inp + "=" + phoneBook[inp]);
-                }
-                else if (!phoneBook.ContainsKey(inp) && inp != "end")
-                {
-                    Console.WriteLine("Not found");
-                }
-            }
+//            }
 
 
-        }
+//            foreach (string inp in queryList)
+//            {
+//                //Console.WriteLine(inp);
+//                if (phoneBook.ContainsKey(inp))
+//                {
+//                    Console.WriteLine(inp + "=" + phoneBook[inp]);
+//                }
+//                else if (!phoneBook.ContainsKey(inp) && inp != "end")
+//                {
+//                    Console.WriteLine("Not found");
+//                }
+//            }
+
+
+//        }
 
 
 
-    }
-}
+//    }
+//}
