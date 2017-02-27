52da44b647c86c182a3d4842c81262471cc2bb3a
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-02-27 22:50:56 +0500

    yeah! completed

--- 30DaysOfCode/Day10.cs
+++ 30DaysOfCode/Day10.cs
@@ -1,71 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace _30DaysOfCode
-{
-    class Day10
-    {
-        static void Main(string[] args)
-        {
-            int N = Convert.ToInt32(Console.ReadLine());
-            List<int> list = new List<int>();
-            int remainder = 0;
-            int totalNumberOfN = 0;
-            int? previousN = null;        // nullable int
-            int temp = 0;
-            while(N>0)
-            {
-                remainder = N % 2;
-                N = N / 2;
-                list.Add(remainder);
-            }
-            list.Reverse();
-            foreach (int i in list)
-            {
-                //Console.Write(i);
-                if (i == 1 && previousN == null)
-                {
-                    temp++;
-                    previousN = i;
-                }
-                else if (i == 0 && previousN==1)
-                {
-                    if(temp>=totalNumberOfN)
-                    {
-                        totalNumberOfN = temp;
-                    }
-                    temp = 0;
-                    previousN = i;
-                }
-                else if(i==i && previousN==0)
-                {
-                    temp++;
-                    if(temp>=totalNumberOfN)
-                    {
-                        totalNumberOfN = temp;
-                    }
-                    previousN = i;
-                }
-                else if(i==0 && previousN ==0)
-                {
-                    continue;
-                }
-                else if(i==1 && previousN==1)
-                {
-                    temp++;
-                    if(temp>=totalNumberOfN)
-                    {
-                        totalNumberOfN = temp;
-                    }
-                    previousN = i;
-                }
+//namespace _30DaysOfCode
+//{
+//    class Day10
+//    {
+//        static void Main(string[] args)
+//        {
+//            int N = Convert.ToInt32(Console.ReadLine());
+//            List<int> list = new List<int>();
+//            int remainder = 0;
+//            int totalNumberOfN = 0;
+//            int? previousN = null;        // nullable int
+//            int temp = 0;
+//            while(N>0)
+//            {
+//                remainder = N % 2;
+//                N = N / 2;
+//                list.Add(remainder);
+//            }
+//            list.Reverse();
+//            foreach (int i in list)
+//            {
+//                //Console.Write(i);
+//                if (i == 1 && previousN == null)
+//                {
+//                    temp++;
+//                    previousN = i;
+//                }
+//                else if (i == 0 && previousN==1)
+//                {
+//                    if(temp>=totalNumberOfN)
+//                    {
+//                        totalNumberOfN = temp;
+//                    }
+//                    temp = 0;
+//                    previousN = i;
+//                }
+//                else if(i==i && previousN==0)
+//                {
+//                    temp++;
+//                    if(temp>=totalNumberOfN)
+//                    {
+//                        totalNumberOfN = temp;
+//                    }
+//                    previousN = i;
+//                }
+//                else if(i==0 && previousN ==0)
+//                {
+//                    continue;
+//                }
+//                else if(i==1 && previousN==1)
+//                {
+//                    temp++;
+//                    if(temp>=totalNumberOfN)
+//                    {
+//                        totalNumberOfN = temp;
+//                    }
+//                    previousN = i;
+//                }
 
 
-            }
-            Console.Write(totalNumberOfN);
-        }
-    }
-}
+//            }
+//            Console.Write(totalNumberOfN);
+//        }
+//    }
+//}
---
+++ 30DaysOfCode/Day11.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int[][] arr = new int[6][];
+        int largest_Maximum = -50;          // there is a reason to set this value to negative 50...
+        for (int arr_i = 0; arr_i < 6; arr_i++)
+        {
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            arr[arr_i] = Array.ConvertAll(arr_temp, Int32.Parse);
+        }
+        for(int i=0;i<arr.Length-2;i++)
+        {
+            for(int j=0;j<arr.Length-2;j++)
+            {
+                //Console.Write(arr[i][j]);
+                //Console.Write(" ");
+                int temp_row = i;
+                int temp_col = j;
+                int local_maximum = 0;
+                for(int a = temp_row;a<=temp_row+2;a++)
+                {
+                    for(int b=temp_col;b<=temp_col+2;b++)
+                    {
+                       if (a == temp_row + 1 && (b == temp_col || b == temp_col + 2))
+                        {
+                            //Console.Write(" ");
+                            continue;
+                        }
+                        //if (a == 0)
+                        //{
+                        //    largest_Maximum += arr[a][b];
+                        //}
+                        local_maximum += arr[a][b];
+                       // Console.Write(arr[a][b]);
+                        //Console.Write(" ");
+                    }
+                   
+                }
+                
+                
+                if (local_maximum >= largest_Maximum)
+                    largest_Maximum = local_maximum;
+               
+            }
+           
+            //Console.Write(" ");
+            //Console.Write(" ");
+            //Console.Write(" ");
+            //Console.Write(" ");
+            //Console.Write(" ");
+        }
+        Console.Write(largest_Maximum);
+        
+    }
+}
