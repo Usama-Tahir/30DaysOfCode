e50d8efdb596a0c382cb759962730a135dad8d55
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-03-01 07:57:38 +0500

    im dumb as fuck

--- 30DaysOfCode/Day13.cs
+++ 30DaysOfCode/Day13.cs
@@ -1,54 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace _30DaysOfCode
-{
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    abstract class Book
-    {
+//namespace _30DaysOfCode
+//{
+//    using System;
+//    using System.Collections.Generic;
+//    using System.IO;
+//    abstract class Book
+//    {
 
-        protected String title;
-        protected String author;
+//        protected String title;
+//        protected String author;
 
-        public Book(String t, String a)
-        {
-            title = t;
-            author = a;
-        }
-        public abstract void display();
+//        public Book(String t, String a)
+//        {
+//            title = t;
+//            author = a;
+//        }
+//        public abstract void display();
 
 
-    }
+//    }
 
-    class MyBook : Book
-    {
-        int price;
-       public MyBook(string title, string author, int p) : base(title,author)
-        {
-            price = p;
-        }
-        public override void display()
-        {
-            //throw new NotImplementedException();
-            Console.WriteLine("Title: " + title);
-            Console.WriteLine("Author: " + author);
-            Console.WriteLine("Price: " + price);
-        }
-    }
-    class Solution
-    {
-        static void Main(String[] args)
-        {
-            String title = Console.ReadLine();
-            String author = Console.ReadLine();
-            int price = Int32.Parse(Console.ReadLine());
-            Book new_novel = new MyBook(title, author, price);
-            new_novel.display();
-        }
-    }
-}
+//    class MyBook : Book
+//    {
+//        int price;
+//       public MyBook(string title, string author, int p) : base(title,author)
+//        {
+//            price = p;
+//        }
+//        public override void display()
+//        {
+//            //throw new NotImplementedException();
+//            Console.WriteLine("Title: " + title);
+//            Console.WriteLine("Author: " + author);
+//            Console.WriteLine("Price: " + price);
+//        }
+//    }
+//    class Solution
+//    {
+//        static void Main(String[] args)
+//        {
+//            String title = Console.ReadLine();
+//            String author = Console.ReadLine();
+//            int price = Int32.Parse(Console.ReadLine());
+//            Book new_novel = new MyBook(title, author, price);
+//            new_novel.display();
+//        }
+//    }
+//}
---
+++ 30DaysOfCode/Day14.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DaysOfCode
+{
+
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+        public Difference(int[] num)
+        {
+            elements = num;
+        }
+        public void computeDifference()
+        {
+            Array.Sort(elements);
+            maximumDifference =  elements[elements.Length - 1]-elements[0];
+        }
+    }
+
+        class Solution
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+}
