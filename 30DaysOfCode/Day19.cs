381944b1050606fed67989dd15696b847e6d1c89
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-03-06 20:38:27 +0500

    completed

--- 30DaysOfCode/Day19.cs
+++ 30DaysOfCode/Day19.cs
@@ -1,38 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace _30DaysOfCode
-{
-    public interface AdvancedArithmetic
-    {
-        int divisorSum(int n);
-    }
-    class Calculator : AdvancedArithmetic
-    {
-       public int divisorSum(int n)
-        {
-            int sum = 0;
-            for(int i=1;i<= n;i++)
-            {
-                if(n%i==0)
-                {
-                    sum += i;
-                }
-            }
-            return sum;
-        }
-    }
-    class Day19
-    {
-        static void Main(string[] args)
-        {
-            int n = Int32.Parse(Console.ReadLine());
-            AdvancedArithmetic myCalculator = new Calculator();
-            int sum = myCalculator.divisorSum(n);
-            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
-        }
-    }
-}
+//namespace _30DaysOfCode
+//{
+//    public interface AdvancedArithmetic
+//    {
+//        int divisorSum(int n);
+//    }
+//    class Calculator : AdvancedArithmetic
+//    {
+//       public int divisorSum(int n)
+//        {
+//            int sum = 0;
+//            for(int i=1;i<= n;i++)
+//            {
+//                if(n%i==0)
+//                {
+//                    sum += i;
+//                }
+//            }
+//            return sum;
+//        }
+//    }
+//    class Day19
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = Int32.Parse(Console.ReadLine());
+//            AdvancedArithmetic myCalculator = new Calculator();
+//            int sum = myCalculator.divisorSum(n);
+//            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+//        }
+//    }
+//}
