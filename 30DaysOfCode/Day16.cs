fd5b0a9a242a9cb78de0da3c2003120587979716
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-03-06 08:14:47 +0500

    yeah! completed

---
+++ 30DaysOfCode/Day16.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DaysOfCode
+{
+    class Day16
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            int N;
+            try
+            {
+                N = int.Parse(S);
+                Console.WriteLine(N);
+            }
+            catch
+            {
+                Console.WriteLine("Bad String");
+            }
+            
+        }
+    }
+}
