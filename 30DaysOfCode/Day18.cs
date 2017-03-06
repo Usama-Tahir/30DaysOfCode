18d6d38aeca592f8082f3834b01db7f6689b775f
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-03-06 08:51:22 +0500

    completed

---
+++ 30DaysOfCode/Day18.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//// only implemented functionality needed to be implemented...
+//// Remaining code is at HackerRank (30 Days of Code Day 18)
+//namespace _30DaysOfCode
+//{
+
+//    class Day18
+//    {
+//        Queue<char> collection = new Queue<char>();
+//        Stack<char> stack = new Stack<char>();
+//        public void pushCharacter(char ch)
+//        {
+//            stack.Push(ch);
+//        }
+//        public void enqueueCharacter(char ch)
+//        {
+//            collection.Enqueue(ch);
+//        }
+//        public char popCharacter()
+//        {
+//            return stack.Pop();
+//        }
+//        public char dequeueCharacter()
+//        {
+//            return collection.Dequeue();
+//        }
+//    }
+//}
