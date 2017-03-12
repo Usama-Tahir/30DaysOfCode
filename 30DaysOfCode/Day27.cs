bba1b57ef1c38ce187560455e36d09e907e3d3a2
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-03-13 00:13:45 +0500

    completed

--- 30DaysOfCode/Day27.cs
+++ 30DaysOfCode/Day27.cs
@@ -1,26 +1,22 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
 
-//namespace _30DaysOfCode
-//{
-//    class Day27
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine('5');
-//            Console.WriteLine("4 3");
-//            Console.WriteLine("-1 -3 4 2");
-//            Console.WriteLine("5 2");
-//            Console.WriteLine("0 -1 2 1 4");
-//            Console.WriteLine("3 1");
-//            Console.WriteLine("0 -1 -2");
-//            Console.WriteLine("4 3");
-//            Console.WriteLine("1 1 0 -6");
-//            Console.WriteLine("6 4");
-//            Console.WriteLine("4 3 0 0 -3 -4");
-//        }
-//    }
-//}
+
+
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine('5');
+            Console.WriteLine("4 3");
+            Console.WriteLine("-1 3 0 2");
+            Console.WriteLine("5 2");
+            Console.WriteLine("0 -1 2 1 4");
+            Console.WriteLine("4 3");
+            Console.WriteLine("0 -1 2 4");
+            Console.WriteLine("5 2");
+            Console.WriteLine("1 1 0 -6");
+            Console.WriteLine("6 4");
+            Console.WriteLine("4 3 6 0 -3 -4");
+        }
+    }
+
