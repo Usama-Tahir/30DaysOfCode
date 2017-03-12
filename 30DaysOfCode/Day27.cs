057b7522203a97256b8fe7ad412903dd7be610fd
Usama-Tahir <14beseutahir@seecs.edu.pk>
2017-03-13 01:45:46 +0500

    yeah! completed

--- 30DaysOfCode/Day27.cs
+++ 30DaysOfCode/Day27.cs
@@ -1,22 +1,22 @@
-﻿using System;
+﻿//using System;
 
 
 
-    class Solution
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine('5');
-            Console.WriteLine("4 3");
-            Console.WriteLine("-1 3 0 2");
-            Console.WriteLine("5 2");
-            Console.WriteLine("0 -1 2 1 4");
-            Console.WriteLine("4 3");
-            Console.WriteLine("0 -1 2 4");
-            Console.WriteLine("5 2");
-            Console.WriteLine("1 1 0 -6");
-            Console.WriteLine("6 4");
-            Console.WriteLine("4 3 6 0 -3 -4");
-        }
-    }
+//    class Solution
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine('5');
+//            Console.WriteLine("4 3");
+//            Console.WriteLine("-1 3 0 2");
+//            Console.WriteLine("5 2");
+//            Console.WriteLine("0 -1 2 1 4");
+//            Console.WriteLine("4 3");
+//            Console.WriteLine("0 -1 2 4");
+//            Console.WriteLine("5 2");
+//            Console.WriteLine("1 1 0 -6");
+//            Console.WriteLine("6 4");
+//            Console.WriteLine("4 3 6 0 -3 -4");
+//        }
+//    }
 
